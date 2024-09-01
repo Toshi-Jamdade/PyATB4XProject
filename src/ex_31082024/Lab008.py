95e1609c3509ba367d76cc2925cefb641a8e3098
Toshi-Jamdade <jamdadetoshi21@gmail.com>
2024-09-01 17:01:10 +0530

    Adding Lab009

---
+++ src/ex_31082024/Lab008.py
@@ -0,0 +1,44 @@
+# Literals
+# variable_name = variable_value
+# variable_name -> identifier
+# = -> assignment operator
+# variable_value -> literal
+
+age = 65 # int
+
+# decimal system -> base -> 10
+
+#binary -> base -> 2
+binary_number = 0b1010 #10
+
+#octal -> base -> 8
+octal = 0o130 #88
+
+#Hex -> base -> 16
+d = 0x12c
+
+pi = 3.14 #float
+time = False #bool
+
+# list []
+# tuple ()
+
+# Escape Sequence
+print("Hello World")
+print("Hello\nWorld") #\n -> newline
+print("Hello\tWorld") #\t -> tabline
+print("Hello\bWorld") #\b -> backspace
+
+#dir = 'C:\toshi\n.txt'  -- gives error
+dir = r'C:\toshi\n.txt'  # r means raw string
+dir1 = r"C:\toshi\n.txt"
+print(dir)          #C:\toshi\n.txt
+print(dir1)         #C:\toshi\n.txt
+
+# name1 = 'toshi'jamdade' -- gives error
+# \ - Escape Sequence - single
+name1 = 'toshi\'jamdade'
+print(name1)            #toshi'jamdade
+name2 = "toshi'jamdade"
+print(name2)            #toshi'jamdade
+
---
+++ src/ex_31082024/Lab009.py
@@ -0,0 +1,57 @@
+# Operators
+
+# Assignment Operator -> = which is used to store the right value to the left value ref.
+age = 24
+print(age)      #24
+
+# - -> unary operator
+how_many_lambo_do_u_have = -1
+print(how_many_lambo_do_u_have)         #-1
+
+# Arithematic Operators
+# +,-,*,/
+print(2+2)
+print(2-2)
+print(2*2)
+print(2/2)
+
+# x//y  - Gives Quotient
+# x%y   - Gives Remainder
+# x**y  - Power
+# ==    - Compare
+
+print(3//2)     # 1 -> Quotient
+print(7//5)     # 1 -> Quotient
+
+print(7%5)      # 2 -> Remainder
+
+print(2**2)     # 4 -> Power
+print(2**3)     # 8 -> Power
+
+print(2==2)     # True
+print(2==3)     # False
+
+# not operator --only works with boolean.
+# In Python we use "not".
+# "!" is used in JAVA.
+is_pramod_married = True
+print(not is_pramod_married)        #False
+
+# Logical Operators -> Bool
+# <, >, <=, >=
+x=10
+y=20
+print(x>y)      #False
+print(x<y)      #True
+
+a=10
+b=10
+print(a>=b)     #True
+print(a==b)     #True
+
+# OR & AND
+f=False
+t=True
+print(f or t)       #True
+print(f and t)      #False
+
